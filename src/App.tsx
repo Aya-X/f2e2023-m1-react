19e3185512f647926d41891b22dcdbea9b02c959
Aya <90342033+Aya-X@users.noreply.github.com>
2023-11-13 18:21:28 +0800

    feat: Add ScrollTop component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 
+import Fab from '@mui/material/Fab';
+
+import ScrollTop from './components/ScrollTop';
+
 function App(): JSX.Element {
   return (
     <>
       <CssBaseline />
 
       <Container sx={{ maxWidth: '1184px' }} maxWidth={false}>
+          <Toolbar id="back-to-top-anchor" />
         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
       </Container>
+
+      <ScrollTop>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          {/* <KeyboardArrowUpIcon /> */}
+          <p>UP</p>
+        </Fab>
+      </ScrollTop>
     </>
   );
 }
---
+++ src/components/ScrollTop.tsx
@@ -0,0 +1,41 @@
+import Box from '@mui/material/Box';
+
+import Zoom from '@mui/material/Zoom';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+
+interface Props {
+  children: React.ReactElement;
+}
+
+export default function ScrollTop(props: Props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+}
